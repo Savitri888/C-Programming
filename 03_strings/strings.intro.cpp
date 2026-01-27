4d19ef09266a5c16c05b8a86925240e64664fd0f
Savitri888 <sharkhu3649@floridapoly.edu>
2026-01-27 05:24:21 +0000

    Create String Program

--- 03_strings/strings.intro.cpp
+++ 03_strings/strings.intro.cpp
@@ -5,18 +5,18 @@ int main(void){
 
     // Create a string
 
-    std::string str = "Hello";
+    std::string str = "Hello ";
 
     std::cout << str << std::endl;
 
-    std::cout << "The size of the string is" << str.size() << std:endl;
+    std::cout << "The size of the string is " << str.size() << std::endl;
 
     // Concatename strings
-    std += "World";
+    str += "World";
 
     std::cout << str << std::endl;
 
-    std::cout << "The size of the string is" << str.size() << std:endl;
+    std::cout << "The size of the string is" << ' ' << str.size() << std::endl;
 
     return 0;
 }
